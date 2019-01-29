da0c168239bfeed37de68e7ba327ef0eba764c70
sinhrks <sinhrks@gmail.com>
2019-01-30 07:18:19 +0900

    REL: v0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PACKAGE = 'daskperiment'
 README = 'README.rst'
 REQUIREMENTS = 'requirements.txt'
 
-VERSION = '0.2.0.dev0'
+VERSION = '0.1.1'
 
 DESCRIPTION = ('A lightweight tool to perform reproducible machine learning '
                'experiment using Dask.')
